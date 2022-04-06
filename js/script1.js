57b1cd0fed90f99bedb46fb3152dddee2830ecbf
K-Pro <komil.tj@yandex.ru>
2022-04-06 16:34:24 +0500

    map

--- js/script1.js
+++ js/script1.js
@@ -1,4 +1,13 @@
 $( document ).ready(function() {
     $("#year").text( "2021 - " + new Date().getFullYear());
 
+    var map = L.map('smap').setView([51.505, -0.09], 13);
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'kDKwNMrfrsFeWXuszSdUX5cM2_dwQBM4zTt40Dcw1qI'
+}).addTo(map);
 });
