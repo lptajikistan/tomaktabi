f9ac34812994342a4fc7ea21b7bd0551afbc5d20
K-Pro <komil.tj@yandex.ru>
2022-04-07 11:05:48 +0500

    js check fix

--- js/script1.js
+++ js/script1.js
@@ -1,13 +1,39 @@
 $( document ).ready(function() {
+
     $("#year").text( "2021 - " + new Date().getFullYear());
 
-    var map = L.map('smap').setView([51.505, -0.09], 13);
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'UwfWoVpE_cioaFr-Fa0Y-LLbfabiuBFJfwrnABSbAKc' //kDKwNMrfrsFeWXuszSdUX5cM2_dwQBM4zTt40Dcw1qI
-}).addTo(map);
+	if($('#myMap').length>0) {
+		
+		var mymap = L.map('myMap').setView([40.2735, 69.6392], 14);
+
+		L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+			maxZoom: 18,
+			attribution: 'Demo @ 2021',
+			id: 'mapbox/streets-v11',
+			tileSize: 512,
+			zoomOffset: -1
+		}).addTo(mymap);
+
+		//	L.marker([51.5, -0.09]).addTo(mymap)
+		//	.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+
+		var popup = L.popup();
+
+		var marker1 = new L.marker(mymap.getCenter()).addTo(mymap).bindPopup(popup);
+
+		function onMapClick(e) {
+
+			popup
+			//	.setLatLng(e.latlng)
+				.setContent("Координаты точки: " + e.latlng.lat.toFixed(4).toString() + ', ' + e.latlng.lng.toFixed(4).toString())
+			//	.openOn(mymap);
+
+			marker1.setLatLng(e.latlng).openPopup();
+
+		}
+
+		mymap.on('click', onMapClick);
+		document.getElementsByClassName( 'leaflet-control-attribution' )[0].style.display = 'none';
+	}
+
 });
