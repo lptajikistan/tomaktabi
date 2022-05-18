da32464b1f92cef669b0c3a98ec39e8111c075f1
K-Pro <komil.tj@yandex.ru>
2022-05-18 18:12:39 +0500

    video modal

--- js/script1.js
+++ js/script1.js
@@ -3,4 +3,23 @@ $( document ).ready(function() {
 	let c_year = new Date().getFullYear();
     $("#year").text( "2021 - " + c_year);
 
+    var $videoSrc;  
+    $('.video-btn').click(function() {
+        $videoSrc = $(this).data( "src" );
+    });
+
+    // when the modal is opened autoplay it  
+    $('#myModal').on('shown.bs.modal', function (e) {
+        // set the video src to autoplay and not to show related video. 
+        // Youtube related video is like a box of chocolates... you never know what you're gonna get
+        $("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"); 
+    })
+      
+    // stop playing the youtube video when I close the modal
+    $('#myModal').on('hide.bs.modal', function (e) {
+        // a poor man's stop video
+        $("#video").attr('src',$videoSrc); 
+    }) 
+        
+
 });
